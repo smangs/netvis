b17b80de907eb400963746f0a08d642a3d02e003
Sascha Mangs <sascha.mangs@yale.edu>
2019-02-05 10:56:44 -0500

    Refactor Connection object into NetConn object.

--- connections/netconn.go
+++ connections/netconn.go
@@ -3,31 +3,31 @@ package Connections
 import (
 	"log"
 
-	"github.com/smangs/netvis/crypto"
+	Crypto "github.com/smangs/netvis/crypto"
 	validator "gopkg.in/go-playground/validator.v9"
 )
 
 // use a single instance of Validate, it caches struct info
 var validate *validator.Validate
 
-// Connection type is used to store Connection objects.
+// NetConn type is used to store Connection objects.
 // ID should be generated with Device.CreateSha256("IP" + "Port") function.
-type Connection struct {
+type NetConn struct {
 	ID   string `validate:"required"`
 	IP   string `validate:"required,ip"`
 	Port string `validate:"required"`
 }
 
-// CreateConnection is used to register a new observed connection for Device.
-// It returns a new Connection object.
-func CreateConnection(connIP string, connPort string) (Connection, error) {
+// CreateNetConn is used to register a new observed connection for Device.
+// It returns a new NetConn object.
+func CreateNetConn(connIP string, connPort string) (NetConn, error) {
 	// TODO validate Port format "tcp/22, udp/53"
 
 	// create Sha256 for Connection.ID using netvis.crypto package
-	connID := crypto.CreateSha256(connIP + connPort)
+	connID := Crypto.CreateSha256(connIP + connPort)
 
 	// create initial Connection object.
-	conn := Connection{
+	conn := NetConn{
 		ID:   connID,
 		IP:   connIP,
 		Port: connPort,
@@ -38,7 +38,7 @@ func CreateConnection(connIP string, connPort string) (Connection, error) {
 	err := validate.Struct(conn)
 	if err != nil {
 		log.Println(err)
-		return Connection{}, err
+		return NetConn{}, err
 	}
 
 	return conn, nil
--- crypto/crypto.go
+++ crypto/crypto.go
@@ -1,4 +1,4 @@
-package crypto
+package Crypto
 
 import (
 	"crypto/sha256"
